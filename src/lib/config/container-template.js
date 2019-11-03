be8712e7f92cafa4553539f32cbc2601b1b7041c
Vasily Vasinov <des.elyon@gmail.com>
2019-11-03 10:57:05 -0700

    Add an official Julia image to the list of default images

--- src/lib/config/container-template.js
+++ src/lib/config/container-template.js
@@ -12,7 +12,8 @@ const template = {
         "getvoila/python:0.1.0",
         "getvoila/math-libs:0.1.0",
         "getvoila/gfortran:0.1.0",
-        "getvoila/open-mpi:0.1.0"
+        "getvoila/open-mpi:0.1.0",
+        "julia:1.2.0-buster"
       ],
       "env": [],
       "actions": []
