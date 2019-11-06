d3af7ac392162f697a8c02097c8f00768e21c119
Vasily Vasinov <des.elyon@gmail.com>
2019-11-05 17:52:42 -0700

    Add release.sh for easier releases

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]
+  then
+    echo "Version required, exiting..."
+    exit 1
+else
+  echo "Updating version in package.json..."
+  jq ".version = \"$1\"" package.json > package.$$.json && mv package.$$.json package.json
+
+  echo "Updating README.md..."
+  oclif-dev readme
+
+  echo "Committing and pushing changes to git..."
+  git add .
+  git commit -m "Releasing v$1"
+  git push origin master
+
+  echo "Packing tarballs..."
+  oclif-dev pack -t linux-arm,linux-x64,darwin-x64
+
+  echo "Pushing tarballs to S3"
+  oclif-dev publish
+fi
