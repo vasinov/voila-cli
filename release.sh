9497bf5d720b8685766e08458e7e482008b78747
Vasily Vasinov <des.elyon@gmail.com>
2019-11-05 18:18:28 -0700

    Releasing v0.2.0-beta

--- release.sh
+++ release.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+set -e
+
 if [ -z "$1" ]
   then
     echo "Version required, exiting..."
     exit 1
 else
   echo "Updating version in package.json..."
-  jq ".version = \"$1\"" package.json > package.$$.json && mv package.$$.json package.json || exit 1
+  jq ".version = \"$1\"" package.json > package.$$.json && mv package.$$.json package.json
 
   echo "Updating README.md..."
-  oclif-dev readme || exit 1
+  oclif-dev readme
 
   if [ ! -z "$(git status --porcelain)" ]
     then
       echo "Committing and pushing changes to git..."
-      git add . || exit 1
-      git commit -m "Releasing v$1" || exit 1
-      git push origin master || exit 1
+      git add .
+      git commit -m "Releasing v$1"
+      git push origin master
   fi
 
   echo "Packing tarballs..."
-  oclif-dev pack -t linux-arm,linux-x64,darwin-x64
+  oclif-dev pack
 
   echo "Pushing tarballs to S3"
   oclif-dev publish
