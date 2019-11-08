895deb3758d78659d689f35aa725ea90cf49df54
Vasily Vasinov <des.elyon@gmail.com>
2019-11-08 15:43:49 -0700

    If there's just one module then default to it

--- src/lib/tasks/index.js
+++ src/lib/tasks/index.js
@@ -5,14 +5,18 @@ exports.loadConfig = require('./load-config').task
 exports.parseConfig = require('./parse-config').task
 
 exports.executeModuleAction = (ctx, flags, args, action) => {
+  const modules = ctx.config.modules
+
   if (flags['all']) {
-    ctx.config.modules.map((module) => {
+    modules.map((module) => {
       action(ctx, module)
     })
   } else if (args.module) {
     action(ctx, ctx.config.getModule(args.module))
   } else if (flags['module-name']) {
     action(ctx, ctx.config.getModule(flags['module-name']))
+  } else if (modules.length === 1) {
+    action(ctx, modules[0])
   } else {
     throw new VoilaError(errorMessages.SPECIFY_MODULE_NAME)
   }
