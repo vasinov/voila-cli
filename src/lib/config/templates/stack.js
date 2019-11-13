d802fca26e396735134c677d9ee3cb4da5fe45bb
Vasily Vasinov <des.elyon@gmail.com>
2019-11-13 13:30:26 -0700

    Update the default stack template

--- src/lib/config/templates/stack.js
+++ src/lib/config/templates/stack.js
@@ -1,21 +1,11 @@
 exports.stackTemplate = (name, images) => {
   return {
     "name": name,
-    "env": [],
     "hostDir": ".",
     "containerDir": "/project",
-    "volumes": [],
-    "ports": [],
     "stages": {
       "build": {
-        "images": images,
-        "env": [],
-        "actions": [
-          { "execute": "apt-get update" }
-        ]
-      },
-      "run": {
-        "command": ""
+        "images": images
       }
     }
   }
